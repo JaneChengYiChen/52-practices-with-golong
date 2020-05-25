f65134cb250b73ec16149d2b40dd0e3f6a97b44c
janecheng <jane.zheng@leishan.com.tw>
2020-05-25 16:35:20 +0800

    cutty introduction

--- hello.go
+++ hello.go
@@ -7,11 +7,14 @@ import (
 	"io"
 	"math/rand"
 	"net/http"
+	"strings"
 )
+
+var tinyMan = "cutty"
 
 // 程式執行入口
 func main() {
-	start()
+	start(tinyMan)
 	habit()
 
 	// router : fuc
@@ -21,18 +24,35 @@ func main() {
 }
 
 // ref https://blog.kdchang.cc/2017/07/01/golang101-tutorial-introduction/
-func start() {
+func start(name string) {
 	greetings := "Hello, Jane!"
-	introduction := "My name is ___, is there anything I can help you?"
-	fmt.Println(greetings, introduction)
+	introduction := fmt.Sprintf("%s %s", "I am", name)
+	test := strings.Join([]string{"Your", "BEST", "Friend"}, "-")
+	fmt.Println(greetings, introduction, test)
 }
 
 // ref https://blog.kdchang.cc/2017/07/01/golang101-tutorial-introduction/
 func habit() {
-	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Println("My favorite number is", rand.Intn(100))
+	fmt.Println("My habits is", habits_type(rand.Intn(7)))
 }
 
 // ref https://blog.kdchang.cc/2017/07/01/golang101-tutorial-introduction/
 func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello Jane! It's your web page :))")
+}
+
+func habits_type(which int) string {
+	whichHabits := [7]string{
+		"Baseball",
+		"Talk Shows",
+		"Singing",
+		"Jogging",
+		"Dishing",
+		"Fishing",
+		"Cooking",
+	}
+
+	var thisHabit = whichHabits[which]
+	return thisHabit
 }
