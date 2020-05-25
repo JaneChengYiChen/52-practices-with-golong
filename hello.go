da4780a11123bb118f66bdb69d090d8c53163c61
janecheng <jane.zheng@leishan.com.tw>
2020-05-25 15:49:25 +0800

    readme

--- hello.go
+++ hello.go
@@ -1,38 +1,38 @@
-
 // 宣告程式屬於哪個 package
 package main
 
 // 引入套件
 import (
-    "fmt"
-    "math/rand"
-    "io"
-    "net/http"
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
 )
 
 // 程式執行入口
 func main() {
-    start()
-    habit()
+	start()
+	habit()
 
-    // router 讓網址可以對應處理函式
-    http.HandleFunc("/", hello)
-    // 監聽 8000 port
-    http.ListenAndServe(":8000", nil)
+	// router : fuc
+	http.HandleFunc("/", hello)
+	// listen 8000 port
+	http.ListenAndServe(":8000", nil)
 }
 
 // ref https://blog.kdchang.cc/2017/07/01/golang101-tutorial-introduction/
-func start(){ 
-    greetings := "Hello, Jane!"
-    introduction := "My name is ___, is there anything I can help you?"
-    fmt.Println(greetings, introduction)
+func start() {
+	greetings := "Hello, Jane!"
+	introduction := "My name is ___, is there anything I can help you?"
+	fmt.Println(greetings, introduction)
 }
+
 // ref https://blog.kdchang.cc/2017/07/01/golang101-tutorial-introduction/
 func habit() {
-	fmt.Println("My favorite number is", rand.Intn(10)) 
+	fmt.Println("My favorite number is", rand.Intn(10))
 }
 
 // ref https://blog.kdchang.cc/2017/07/01/golang101-tutorial-introduction/
 func hello(w http.ResponseWriter, r *http.Request) {
-    io.WriteString(w, "Hello Jane! It's your web page :))") 
+	io.WriteString(w, "Hello Jane! It's your web page :))")
 }
